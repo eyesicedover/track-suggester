7ec77be7c2cc1d9013fa0457fde2e12a5f553d6d
Stephanie Faber <eyesicedover@gmail.com>
2018-01-12 12:02:45 -0800

    add counters for survey answers, add partial logic counting user answers to questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,38 @@
+var javaCounter = 0;
+var phpCounter = 0;
+var rubyCounter = 0;
+var cCounter = 0;
+
+
 $(document).ready(function(){
   $("#start").click(function() {
     event.preventDefault();
     $("#start").fadeOut();
     $(".intro").fadeOut();
-    $("#surveyForm").fadeIn();
+    $("#surveyForm").delay(900).fadeIn();
   });
 
   $("#surveyForm").submit(function(event){
     event.preventDefault();
+    var name = $("input#userName").val();
+    var jobDesc = $("input:radio[name=jobDesc]:checked").val();
+    var difficulty = $("input:radio[name=difficulty]:checked").val();
+    var jobOpen = $("input:radio[name=jobOpen]:checked").val();
+    var resources = $("input:radio[name=resources]:checked").val();
+    var whatToDo = $("input:radio[name=whatToDo]:checked").val();
+
+    $(".name").text(name);
+
+    if (name === "Java") {
+      javaCounter++;
+    } else if (name === "C") {
+      cCounter++;
+    } else if (name === "PHP") {
+      phpCounter++;
+    } else {
+      rubyCounter++;
+    }
+
+
   });
 });
